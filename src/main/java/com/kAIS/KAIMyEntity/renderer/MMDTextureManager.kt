1276052ece032ebecabccc3a0d31c01d49655934
GG Bond <whatever@tagkong.com>
2024-01-15 03:58:35 +0800

    Use gln

--- src/main/java/com/kAIS/KAIMyEntity/renderer/MMDTextureManager.kt
+++ src/main/java/com/kAIS/KAIMyEntity/renderer/MMDTextureManager.kt
@@ -2,12 +2,21 @@ package com.kAIS.KAIMyEntity.renderer
 
 import com.kAIS.KAIMyEntity.KAIMyEntityClient
 import com.kAIS.KAIMyEntity.NativeFunc
+import gli_.gl.ExternalFormat.RGBA
+import gli_.gl.InternalFormat.RGBA_UNORM
+import gli_.gl.InternalFormat.RGB_UNORM
+import gli_.gl.TypeFormat.U8
+import glm_.vec2.Vec2i
+import gln.TextureTarget
+import gln.identifiers.GlTexture
+import gln.texture.TexMagFilter
+import gln.texture.TexMinFilter
 import org.lwjgl.opengl.GL46C
 import java.nio.ByteBuffer
 
-object MMDTextureManager {
-    private val textures = mutableMapOf<String, Texture>()
+private val textures = mutableMapOf<String, MMDTextureManager.Texture>()
 
+object MMDTextureManager {
     fun texture(filename: String): Texture {
         return textures.computeIfAbsent(filename) {
             with(NativeFunc) {
@@ -21,52 +30,29 @@ object MMDTextureManager {
                 val texData = GetTextureData(nfTex)
                 val hasAlpha = TextureHasAlpha(nfTex)
 
-                val tex = GL46C.glGenTextures()
-                GL46C.glBindTexture(GL46C.GL_TEXTURE_2D, tex)
-                val texSize = x * y * (if (hasAlpha) 4 else 3)
-                val texBuffer = ByteBuffer.allocateDirect(texSize)
-                CopyDataToByteBuffer(texBuffer, texData, texSize.toLong())
-                if (hasAlpha) {
-                    GL46C.glPixelStorei(GL46C.GL_UNPACK_ALIGNMENT, 4)
-                    GL46C.glTexImage2D(
-                        GL46C.GL_TEXTURE_2D,
-                        0,
-                        GL46C.GL_RGBA,
-                        x,
-                        y,
-                        0,
-                        GL46C.GL_RGBA,
-                        GL46C.GL_UNSIGNED_BYTE,
-                        texBuffer
+                val tex = GlTexture.gen()
+                tex.bound(TextureTarget._2D) {
+                    val texSize = x * y * (if (hasAlpha) 4 else 3)
+                    val texBuffer = ByteBuffer.allocateDirect(texSize)
+                    CopyDataToByteBuffer(texBuffer, texData, texSize.toLong())
+                    val alignment = if (hasAlpha) 4 else 1
+                    GL46C.glPixelStorei(GL46C.GL_UNPACK_ALIGNMENT, alignment)
+                    image2D(
+                        if (hasAlpha) RGBA_UNORM else RGB_UNORM,
+                        Vec2i(x, y), RGBA, U8, texBuffer,
                     )
-                } else {
-                    GL46C.glPixelStorei(GL46C.GL_UNPACK_ALIGNMENT, 1)
-                    GL46C.glTexImage2D(
-                        GL46C.GL_TEXTURE_2D,
-                        0,
-                        GL46C.GL_RGB,
-                        x,
-                        y,
-                        0,
-                        GL46C.GL_RGB,
-                        GL46C.GL_UNSIGNED_BYTE,
-                        texBuffer
-                    )
+                    DeleteTexture(nfTex)
+                    maxLevel = 0
+                    minFilter = TexMinFilter.LINEAR
+                    magFilter = TexMagFilter.LINEAR
                 }
-                DeleteTexture(nfTex)
-
-                GL46C.glTexParameteri(GL46C.GL_TEXTURE_2D, GL46C.GL_TEXTURE_MAX_LEVEL, 0)
-                GL46C.glTexParameteri(GL46C.GL_TEXTURE_2D, GL46C.GL_TEXTURE_MIN_FILTER, GL46C.GL_LINEAR)
-                GL46C.glTexParameteri(GL46C.GL_TEXTURE_2D, GL46C.GL_TEXTURE_MAG_FILTER, GL46C.GL_LINEAR)
-                GL46C.glBindTexture(GL46C.GL_TEXTURE_2D, 0)
-
-                Texture(tex, hasAlpha)
+                Texture(tex.name, hasAlpha)
             }
         }
     }
 
     data class Texture(
         val tex: Int,
-        val hasAlpha: Boolean = false,
+        val hasAlpha: Boolean,
     )
 }
